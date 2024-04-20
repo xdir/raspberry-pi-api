c21f47dfbb6f1a5e81b91c35892331d8d5b6964b
xdir <47368459+xdir@users.noreply.github.com>
2024-04-20 23:02:53 +0300

    Update api.py

--- temperature/api.py
+++ temperature/api.py
@@ -34,7 +34,7 @@ def get_temperatures():
                 "sensor_id": sensor_id,
                 "temperature": temperature
             })
-    t = (time.time() - start) / 1000
+    t = (int)((time.time() - start) * 1000)
     print(f"Elapsed time: {t} ms.")
     return jsonify({"temperatures": temperatures})
 
