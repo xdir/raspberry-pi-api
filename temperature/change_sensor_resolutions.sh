e0e4ba3725ca886dc0ae5c7bf059e021c52e0dd6
xdir <47368459+xdir@users.noreply.github.com>
2024-04-28 23:22:11 +0300

    Update change_sensor_resolutions.sh

--- temperature/change_sensor_resolutions.sh
+++ temperature/change_sensor_resolutions.sh
@@ -4,4 +4,6 @@
 
 echo 9 > /sys/bus/w1/devices/28-031654c65fff/w1_slave
 echo 9 > /sys/bus/w1/devices/28-0214630b7cff/w1_slave
+
+echo 9 > /sys/bus/w1/devices/28-0214633567ff/w1_slave
 echo 9 > /sys/bus/w1/devices/28-00000f9c3848/w1_slave
